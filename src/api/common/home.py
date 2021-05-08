92752aa302e166885ab3e39b3e9d2ac9185a013e
Asa-Nisi-Masa <gytis.brazdziunas@gmail.com>
2021-05-08 21:56:03 +0300

    switched homepage

--- src/api/common/home.py
+++ src/api/common/home.py
@@ -5,4 +5,4 @@ from flask_restful import Resource
 class Home(Resource):
 
     def get(self):
-        return redirect(url_for("ratingsupload"))
+        return redirect(url_for("similarmoviefinder"))
